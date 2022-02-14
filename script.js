ffbb1a3832fac19c186ec8733b084ba1f34bc4d2
andrecunha10 <andre.cunha10@gmail.com>
2022-02-13 22:12:38 -0300

    menu funcionando e adicao do botao carrinho

--- script.js
+++ script.js
@@ -2,9 +2,20 @@ const btnMobile = document.getElementById('btn-mobile');
 
 function toggleMenu(event) {
   if (event.type === 'touchstart') event.preventDefault();
-  const nav = document.getElementById('nav1');
-  nav.classList.toggle('active');
+  const nav1 = document.getElementById('nav1');
+  nav1.classList.toggle('activemenu');
 }
 
 btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu);
+
+const btncart = document.getElementById('btn-cart');
+
+function togglecart(event) {
+    if (event.type ==='touchstart') event.preventDefault();
+    const cart = document.getElementById('cart');
+    cart.classList.toggle('activecart');
+}
+
+btncart.addEventListener('click', togglecart);
+btncart.addEventListener('touchstart', togglecart);
