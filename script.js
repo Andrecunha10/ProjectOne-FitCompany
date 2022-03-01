342b59189d480f31a98602251fe3c7e508a87efc
andrecunha10 <andre.cunha10@gmail.com>
2022-02-28 22:02:03 -0300

    adicionas itens no carrinho. Está com erro na quantidade

--- script.js
+++ script.js
@@ -1,38 +1,34 @@
+//Open Menu Mobile
 const btnMobile = document.getElementById('btn-mobile');
-
 function toggleMenu(event) {
   if (event.type === 'touchstart') event.preventDefault();
   const nav1 = document.getElementById('nav-mobile');
   nav1.classList.toggle('activemenu');
 }
-
 btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu);
 
+//Open/Close Cart - With Cart Button
 const btncart = document.getElementById('btn-cart');
-
 function togglecart(event) {
   if (event.type === 'touchstart') event.preventDefault();
   const cart = document.getElementById('cart-windows');
   cart.classList.toggle('activecart');
 }
-
 btncart.addEventListener('click', togglecart);
 btncart.addEventListener('touchstart', togglecart);
 
+//Close Cart - With Button in Cart Windows
 const btncartclose = document.getElementById('btn-close-cart');
-
-
 function togglecartclose(event) {
   if (event.type === 'touchstart') event.preventDefault();
   const cartclose = document.getElementById('cart-windows');
   cartclose.classList.remove('activecart');
 }
-
 btncartclose.addEventListener('click', togglecartclose);
 btncartclose.addEventListener('touchstart', togglecartclose);
 
-
+//Validation API
 const selectGroups = () => {
   const mainGroups = document.querySelector('#main-groups')
   fetch('/apifake.json')
@@ -50,6 +46,7 @@ const selectGroups = () => {
       mainGroups.innerHTML = '<p class="error">Falha ao carregar a página. Por favor, tente novamente.</p>'
     })
 }
+//ADD HTML FROM API
 const getSectionTag = (group) => {
   const createSection = document.createElement('section')
   const createTitle = document.createElement('h2')
@@ -70,7 +67,86 @@ const getSectionTag = (group) => {
             <button type="button" class="btn-santadart btnaddcart">Adicionar ao carrinho</button>
        </div>
   `
+  const btnAddCart = createArticle.querySelector('.btnaddcart')
+  btnAddCart.addEventListener('click', () => {
+    addToCart(product)
   })
+})
   return createSection
 }
 selectGroups()
+
+const addProductsCart = []
+const addToCart = newProduct => {
+  const productIndex = addProductsCart.findIndex(
+    item => item.id === newProduct.id
+  )
+  if (productIndex === -1) {
+    addProductsCart.push({
+      ...newProduct,
+      qty: 1
+    })
+    const selectDivEmptyCart = document.querySelector ('.empty-cart')
+    selectDivEmptyCart.classList.add('full-cart')
+    const selectBtnRequest = document.querySelector ('.btn-request')
+    selectBtnRequest.classList.add('btn-request-on')
+    const selectAside = document.getElementById('add-products-cart')
+    const createDivProductSelect = document.createElement('div')
+    createDivProductSelect.classList.add('product-select')
+    selectAside.appendChild(createDivProductSelect)
+    addProductsCart.forEach(product => {
+    createDivProductSelect.innerHTML = `
+    <img class="image-cart" src="${product.image}" alt="${product.name}" height="47" width="40" />
+    <p>${product.name}</p>
+    `
+    const createProductsQty = document.createElement('p')
+    createProductsQty.textContent = `(${product.qty})`
+    createProductsQty.classList.add('product-qty')
+    createDivProductSelect.appendChild(createProductsQty)
+    const createBtntrash = document.createElement('button')
+    createBtntrash.innerHTML = `<img src="images/trash.svg" alt="Excluir" height="27" width="22" />`
+    createDivProductSelect.appendChild(createBtntrash)
+    })
+  } else {
+    addProductsCart[productIndex].qty++
+    addProductsCart.forEach(product => {
+      const teste = document.querySelector('.product-qty')
+      teste.textContent = `(${product.qty})`
+    })
+
+  }
+  // getProductToCart()
+  handleCartUpdate()
+
+}
+const handleCartUpdate = () => {
+  if (addProductsCart.length > 0) {
+    const cartBadge = document.querySelector('.btn-cart-badge')
+    cartBadge.classList.add('btn-cart-badge-show')
+    let total = 0
+    addProductsCart.forEach(product => {
+      total = total + product.qty
+    })
+    cartBadge.textContent = total
+  }
+}
+
+// const getProductToCart = () => {
+//   const selectDivEmptyCart = document.querySelector ('.empty-cart')
+//   selectDivEmptyCart.classList.add('full-cart')
+//   const selectBtnRequest = document.querySelector ('.btn-request')
+//   selectBtnRequest.classList.add('btn-request-on')
+//   const selectAside = document.getElementById('add-products-cart')
+//   const createDivProductSelect = document.createElement('div')
+//   createDivProductSelect.classList.add('product-select')
+//   selectAside.appendChild(createDivProductSelect)
+//   addProductsCart.forEach(product => {
+//   createDivProductSelect.innerHTML = `
+//     <img class="image-cart" src="${product.image}" alt="${product.name}" height="47" width="40" />
+//     <p>${product.name}</p>
+//     <p class="produtc-qty">(${parseInt(product.qty)})</p>
+//     <button><img src="images/trash.svg" alt="Excluir" height="27" width="22" /></button>
+//   `
+//   })
+// }
+
